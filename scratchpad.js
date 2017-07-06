97806eb79f3aba18de131780ac5752b0edc08981
Patryk <wolfpoland1@gmail.com>
2017-07-06 14:46:52 +0200

    wersja dzialajaca

--- scratchpad.js
+++ scratchpad.js
@@ -5,22 +5,79 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
+let first=true;
+let firsto=true;
+if(first){
+setTimeout(glowna, 2000);
+}
+function glowna(){
+  first=false;
+var newo=false;
 
 let lol=document.getElementById('guide-channels');
-let element=document.createElement("li");
-element.appendChild(document.createTextNode("Szukaj"));
+
+let element=null;
+let tag=null;
+if (lol == null && document.querySelector('.style-scope ytd-guide-renderer #container') == null){
+  console.log("DZIALA TERMINATOR");
+  let termi=document.getElementById("guide-button");
+  termi.addEventListener('click',()=>{
+    console.log("KLIK");
+    console.log("Stan peirwszego: ");
+    console.log(first);
+    if(firsto){
+      console.log("ODPALAM JESZCZE RAZ");
+    glowna();
+
+    firsto=false;
+  }
+  });
+}else{
+if (lol == null){
+
+  tag=document.createElement("p");
+    tag.appendChild(document.createTextNode("Search"));
+   element=document.createElement("div");
+   element.appendChild(tag);
+  lol=document.querySelector('.style-scope ytd-guide-renderer #container');
+  element.style.margin='0 10%';
+  newo=true;
+
+
+}else{
+
+  element=document.createElement("li");
+  element.appendChild(document.createTextNode("Search"));
+}
+
+
 lol.parentNode.insertBefore(element,lol);
 let inp=document.createElement("input");
 inp.setAttribute('type','text');
 element.append(inp);
 let sub=lol.getElementsByTagName("li");
+if (sub.length == 0){
 
+  sub=lol.getElementsByTagName("ytd-guide-entry-renderer");
+}else{
+
+}
 
 inp.addEventListener('keyup',()=>{
-  let val=inp.value;
-  let licznik=0;
-  for(let n=0;n<sub.length;n++){
 
+  let val=inp.value.toLowerCase();
+  let licznik=0;
+
+  for(let n=0;n<sub.length;n++){
+    if(newo){
+      if(sub[n].querySelector("ytd-guide-entry-renderer  span").innerText.toLowerCase().includes(val)){
+        sub[n].style.display='block';
+        licznik--;
+      }else{
+        sub[n].style.display='none';
+        licznik++;
+      }
+    }else{
     if(sub[n].querySelector("li > a").getAttribute("title").toLowerCase().includes(val)){
       sub[n].style.display='block';
       licznik--;
@@ -28,22 +85,43 @@ inp.addEventListener('keyup',()=>{
       sub[n].style.display='none';
       licznik++;
     }
-    console.log(lol.getElementsByTagName("li").length);
-    
-    if(lol.getElementsByTagName("li").length-licznik == 0){
-      console.log('Weszlo: '+sub.length);
+  }
+
+    if(newo){
+    if(lol.getElementsByTagName("ytd-guide-entry-renderer").length-licznik == 0){
+
       let tra=document.createElement('img');
       tra.setAttribute('src','http://goo.gl/d2kCy6');
       tra.setAttribute('id','trav');
       element.append(tra);
-      
+
     }else{
-      
+
       let testt=document.getElementById('trav');
       if(testt != null){
         testt.remove();
       }
-     
+
+    }
+  }else{
+    if(lol.getElementsByTagName("li").length-licznik == 0){
+
+      let tra=document.createElement('img');
+      tra.setAttribute('src','http://goo.gl/d2kCy6');
+      tra.setAttribute('id','trav');
+      element.append(tra);
+
+    }else{
+
+      let testt=document.getElementById('trav');
+      if(testt != null){
+        testt.remove();
+      }
+
     }
   }
+
+  }
 });
+}
+}
