51c44a14629e6c1a6ee07bba746841372bfa9bca
Patryk <wolfpoland1@gmail.com>
2017-12-18 19:42:31 +0100

    backup

--- scratchpad.js
+++ scratchpad.js
@@ -5,42 +5,43 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-let first=true;
-let firsto=true;
-if(first){
-setTimeout(glowna, 800);
+function sleep(ms){
+  return new Promise(res=> setTimeout(res, ms));
 }
-function glowna(){
+
+glowna();
+
+async function glowna(){
   first=false;
 var newo=false;
 let checko=true;
-let lol=document.getElementById('guide-channels');
+let lol=document.querySelector('#guide-renderer #sections');
 
 let element=null;
 let tag=null;
-if (lol == null && document.querySelector('.style-scope ytd-guide-renderer #container') == null){
-  console.log("DZIALA TERMINATOR");
-  firsto=true;
-  checko=false;
-  let termi=document.getElementById("guide-button");
-  termi.addEventListener('click',()=>{
-    console.log("KLIK");
-    console.log("Stan peirwszego: ");
-    console.log(firsto);
-    if(firsto){
-      console.log("ODPALAM JESZCZE RAZ");
-    glowna();
-    console.log("Stan checko");
-    console.log(checko);
-    if(checko){
-      firsto=false;
-      checko=true;
-    }
+if ( document.querySelector('#guide-renderer #sections')== null || document.querySelector('#guide-renderer #sections').childNodes[2] == undefined){
+  
+while( document.querySelector('#guide-renderer #sections')== null || document.querySelector('#guide-renderer #sections').childNodes[2] == undefined ){
+  await sleep(500);
 
-  }
-  });
-}else{
-if (lol == null){
+
+}
+
+}
+if(document.querySelector('#guide-renderer #sections') != null){
+
+  lol=document.querySelector('#guide-renderer #sections');
+
+  lol=lol.childNodes[2];
+  lol=lol.childNodes[5];
+
+
+ element=document.createElement("div");
+
+
+element.setAttribute('style','margin-right: 16%;margin-left: 6%;margin-bottom: 8%;');
+}
+else if (lol == null){
 
   tag=document.createElement("p");
     tag.appendChild(document.createTextNode("Search"));
@@ -61,6 +62,10 @@ if (lol == null){
 lol.parentNode.insertBefore(element,lol);
 let inp=document.createElement("input");
 inp.setAttribute('type','text');
+inp.setAttribute('placeholder','Search');
+
+inp.setAttribute('style','font-family: "Roboto", "Droid Sans", sans-serif;width:100%;font-size: 16px;margin: 0;padding: 8px 8px 6px 8px;position: relative;display: block;outline: none;border: none;background: none;color: #212121;border-bottom: 1px solid #e0e0e0; border-radius: 0;');
+
 element.append(inp);
 let sub=lol.getElementsByTagName("li");
 if (sub.length == 0){
@@ -76,59 +81,21 @@ inp.addEventListener('keyup',()=>{
   let licznik=0;
 
   for(let n=0;n<sub.length;n++){
-    if(newo){
-      if(sub[n].querySelector("ytd-guide-entry-renderer  span").innerText.toLowerCase().includes(val)){
+   
+      if(sub[n].querySelector("ytd-guide-entry-renderer  a").innerText.toLowerCase().includes(val)){
         sub[n].style.display='block';
         licznik--;
       }else{
         sub[n].style.display='none';
         licznik++;
       }
-    }else{
-    if(sub[n].querySelector("li > a").getAttribute("title").toLowerCase().includes(val)){
-      sub[n].style.display='block';
-      licznik--;
-    }else{
-      sub[n].style.display='none';
-      licznik++;
-    }
-  }
+    
 
-    if(newo){
-    if(lol.getElementsByTagName("ytd-guide-entry-renderer").length-licznik == 0){
+ 
 
-      let tra=document.createElement('img');
-      tra.setAttribute('src','http://goo.gl/d2kCy6');
-      tra.setAttribute('id','trav');
-      element.append(tra);
-
-    }else{
-
-      let testt=document.getElementById('trav');
-      if(testt != null){
-        testt.remove();
-      }
-
-    }
-  }else{
-    if(lol.getElementsByTagName("li").length-licznik == 0){
-
-      let tra=document.createElement('img');
-      tra.setAttribute('src','http://goo.gl/d2kCy6');
-      tra.setAttribute('id','trav');
-      element.append(tra);
-
-    }else{
-
-      let testt=document.getElementById('trav');
-      if(testt != null){
-        testt.remove();
-      }
-
-    }
-  }
+  
 
   }
 });
-}
+
 }
