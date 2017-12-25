c4460d247be56b72b66defb4e1d2687ce816d5c8
wolfpoland <wolfpoland1@gmail.com>
2017-12-25 16:40:15 +0100

    update wygladu

--- scratchpad.js
+++ scratchpad.js
@@ -31,7 +31,7 @@ while( document.querySelector('#guide-renderer #sections')== null || document.qu
 if(document.querySelector('#guide-renderer #sections') != null){
   let expand=document.querySelectorAll('#expander-item')[1];
   expand.click();
-  console.log(expand);
+  expand.setAttribute('style','display: none;');
   lol=document.querySelector('#guide-renderer #sections');
 
   lol=lol.childNodes[2];
@@ -39,9 +39,15 @@ if(document.querySelector('#guide-renderer #sections') != null){
 
 
  element=document.createElement("div");
+ element.setAttribute('style',`
+          position: relative;
+          width: 100%;
+          margin-right: 16%;
+          margin-left: 6%;
+          margin-bottom: 8%;
+      `);
 
 
-element.setAttribute('style','margin-right: 16%;margin-left: 6%;margin-bottom: 8%;');
 }
 else if (lol == null){
 
@@ -67,8 +73,10 @@ inp.setAttribute('type','text');
 inp.setAttribute('placeholder','Search');
 
 inp.setAttribute('style','font-family: "Roboto", "Droid Sans", sans-serif;width:100%;font-size: 16px;margin: 0;padding: 8px 8px 6px 8px;position: relative;display: block;outline: none;border: none;background: none;color: #212121;border-bottom: 1px solid #e0e0e0; border-radius: 0;');
-
+let span=document.createElement("span");
+span.setAttribute('style',"position: absolute;  left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;")
 element.append(inp);
+element.append(span);
 let sub=lol.getElementsByTagName("li");
 if (sub.length == 0){
 
@@ -76,7 +84,17 @@ if (sub.length == 0){
 }else{
 
 }
-
+inp.addEventListener('focus',()=>{
+  span.setAttribute('style',`position: absolute;
+        height: 2px;
+        background-color: #3399FF;
+        width: 100%;
+        transition: 0.4s;
+        left: 0;`);
+});
+inp.addEventListener('focusout',()=>{
+span.setAttribute('style',"position: absolute;  left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;");
+});
 inp.addEventListener('keyup',()=>{
 
   let val=inp.value.toLowerCase();
