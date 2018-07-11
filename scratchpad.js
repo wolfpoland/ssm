aab5a53e9b3b0872693a91a573600230a1b51ada
wolfpoland <wolfpoland1@gmail.com>
2018-07-11 21:34:16 +0200

    related to you tube structure changes

--- scratchpad.js
+++ scratchpad.js
@@ -5,93 +5,100 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-function sleep(ms){
-  return new Promise(res=> setTimeout(res, ms));
+function sleep(ms) {
+  return new Promise(res => setTimeout(res, ms));
 }
 
 glowna();
 
-async function glowna(){
+async function glowna() {
+  var newo = false;
+  let checko = true;
+  let lol = document.querySelector('#guide-renderer #sections');
 
-var newo=false;
-let checko=true;
-let lol=document.querySelector('#guide-renderer #sections');
-
-let element=null;
-let tag=null;
-if ( document.querySelector('#guide-renderer #sections')== null || document.querySelector('#guide-renderer #sections').childNodes[2] == undefined){
-
-while( document.querySelector('#guide-renderer #sections')== null || document.querySelector('#guide-renderer #sections').childNodes[2] == undefined ){
-  await sleep(500);
-
-
-}
-
-}
-if(document.querySelector('#guide-renderer #sections') != null){
-  // //const expand=document.querySelectorAll('#expander-item');
-  // const deleteo=document.querySelector('#expander-item a[title="history"]');
-  // console.log(expand)
-  // console.log(deleteo);
-  // if(expand.length > 1){
-  //   let tmp=expand[1];
-  //   tmp.click();
-  //   tmp.setAttribute('style',`
-  //            display: none;
-  //   `);
-  // }else if(expand.length == 1){
-  //   let tmp=expand[0];
-  //   tmp.click();
-  //   tmp.setAttribute('style',`
-  //            display: none;
-  //   `);
-  // }
-
-  lol=document.querySelector('#guide-renderer #sections');
-
-  lol=lol.childNodes[2];
-  lol=lol.childNodes[5];
-  const expand=lol.querySelector('#expander-item');
-  if(expand){
-    expand.click();
-    expand.setAttribute('style',`
-             display: none;
-    `);
+  let element = null;
+  let tag = null;
+  if (
+    document.querySelector('#guide-renderer #sections') == null ||
+    document.querySelector('#guide-renderer #sections').childNodes[2] ==
+      undefined
+  ) {
+    while (
+      document.querySelector('#guide-renderer #sections') == null ||
+      document.querySelector('#guide-renderer #sections').childNodes[2] ==
+        undefined
+    ) {
+      await sleep(500);
+    }
   }
- element=document.createElement("div");
- element.setAttribute('style',`
+  if (document.querySelector('#guide-renderer #sections') != null) {
+    // //const expand=document.querySelectorAll('#expander-item');
+    // const deleteo=document.querySelector('#expander-item a[title="history"]');
+    // console.log(expand)
+    // console.log(deleteo);
+    // if(expand.length > 1){
+    //   let tmp=expand[1];
+    //   tmp.click();
+    //   tmp.setAttribute('style',`
+    //            display: none;
+    //   `);
+    // }else if(expand.length == 1){
+    //   let tmp=expand[0];
+    //   tmp.click();
+    //   tmp.setAttribute('style',`
+    //            display: none;
+    //   `);
+    // }
+
+    lol = document.querySelector('#guide-renderer #sections');
+
+    lol = lol.childNodes[2];
+
+    const expand = lol.querySelector('#expander-item');
+    if (expand) {
+      expand.click();
+      expand.setAttribute(
+        'style',
+        `
+             display: none;
+    `
+      );
+    }
+    element = document.createElement('div');
+    element.setAttribute(
+      'style',
+      `
           width: 100%;
           padding: 0 24px;
           box-sizing: border-box;
           align-items: center;
-      `);
+      `
+    );
+  }
+  // else if (lol == null){
+  //   console.log("Zaszedl ELSE IF");
+  //   tag=document.createElement("p");
+  //     tag.appendChild(document.createTextNode("Search"));
+  //    element=document.createElement("div");
+  //    element.appendChild(tag);
+  //   lol=document.querySelector('.style-scope ytd-guide-renderer #container');
+  //   element.style.margin='0 10%';
+  //   newo=true;
 
+  // }else{
 
-}
-// else if (lol == null){
-//   console.log("Zaszedl ELSE IF");
-//   tag=document.createElement("p");
-//     tag.appendChild(document.createTextNode("Search"));
-//    element=document.createElement("div");
-//    element.appendChild(tag);
-//   lol=document.querySelector('.style-scope ytd-guide-renderer #container');
-//   element.style.margin='0 10%';
-//   newo=true;
+  //   element=document.createElement("li");
+  //   element.appendChild(document.createTextNode("Search"));
+  // }
 
+  lol.parentNode.insertBefore(element, lol);
+  let inp = document.createElement('input');
+  inp.setAttribute('type', 'text');
+  inp.setAttribute('placeholder', 'Search');
 
-// }else{
-
-//   element=document.createElement("li");
-//   element.appendChild(document.createTextNode("Search"));
-// }
-
-
-lol.parentNode.insertBefore(element,lol);
-let inp=document.createElement("input");
-inp.setAttribute('type','text');
-inp.setAttribute('placeholder','Search');
-
-inp.setAttribute('style',`
+  inp.setAttribute(
+    'style',
+    `
   font-family: "Roboto", "Droid Sans", sans-serif;
   width:90%;
   font-size: 16px;
@@ -105,57 +112,64 @@ inp.setAttribute('style',`
   color: var(--yt-primary-color);
   border-bottom: 1px solid #e0e0e0;
   border-radius: 0;
- `);
-let span=document.createElement("span");
-span.setAttribute('style',"position: absolute;  left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;");
-let posredni=document.createElement('div');
-posredni.setAttribute('style',`
+ `
+  );
+  let span = document.createElement('span');
+  span.setAttribute(
+    'style',
+    'position: absolute;  left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;'
+  );
+  let posredni = document.createElement('div');
+  posredni.setAttribute(
+    'style',
+    `
   width: 100%;
   position: relative;
   margin-bottom: 14px;
-`);
-posredni.append(inp);
-posredni.append(span);
-element.append(posredni);
-let sub=lol.getElementsByTagName("li");
-if (sub.length == 0){
-
-  sub=lol.getElementsByTagName("ytd-guide-entry-renderer");
-}else{
-
-}
-inp.addEventListener('focus',()=>{
-  span.setAttribute('style',`position: absolute;
+`
+  );
+  posredni.append(inp);
+  posredni.append(span);
+  element.append(posredni);
+  let sub = lol.getElementsByTagName('li');
+  if (sub.length == 0) {
+    sub = lol.getElementsByTagName('ytd-guide-entry-renderer');
+  } else {
+  }
+  inp.addEventListener('focus', () => {
+    span.setAttribute(
+      'style',
+      `position: absolute;
         height: 2px;
         background-color: #3399FF;
         width: 100%;
         transition: 0.4s;
-        left: 0;`);
-});
-inp.addEventListener('focusout',()=>{
-span.setAttribute('style',"position: absolute;  left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;");
-});
-inp.addEventListener('keyup',()=>{
+        left: 0;`
+    );
+  });
+  inp.addEventListener('focusout', () => {
+    span.setAttribute(
+      'style',
+      'position: absolute;  left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;'
+    );
+  });
+  inp.addEventListener('keyup', () => {
+    let val = inp.value.toLowerCase();
+    let licznik = 0;
 
-  let val=inp.value.toLowerCase();
-  let licznik=0;
-
-  for(let n=0;n<sub.length;n++){
-
-      if(sub[n].querySelector("ytd-guide-entry-renderer  a").innerText.toLowerCase().includes(val)){
-        sub[n].style.display='block';
+    for (let n = 0; n < sub.length; n++) {
+      if (
+        sub[n]
+          .querySelector('ytd-guide-entry-renderer  a')
+          .innerText.toLowerCase()
+          .includes(val)
+      ) {
+        sub[n].style.display = 'block';
         licznik--;
-      }else{
-        sub[n].style.display='none';
+      } else {
+        sub[n].style.display = 'none';
         licznik++;
       }
-
-
-
-
-
-
-  }
-});
-
+    }
+  });
 }
